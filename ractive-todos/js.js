e38ac6c057af3ba5e653a84dd6cc4f95ce4cc076
Tung Nguyen Son <tung.nguyen.son@ntq-solution.com.vn>
2016-06-24 12:12:03 +0700

    Update code

--- ractive-todos/js.js
+++ ractive-todos/js.js
@@ -2,16 +2,20 @@
 var FormAdd = Ractive.extend({
 	template: '#form-add-template',
 	data: {
-		item: '',
+		desc: '',
 		selectAll: false,
 	},
-	submit: function(event) {
+	formSubmit: function(event) {
 		event.original.preventDefault();
 
-		if ( ! this.get('item')) return;
+		if ( ! this.get('desc')) return;
 
-		this.fire('addItem', this.get('item'));
-		this.set('item', '');
+		this.fire('addItem', {
+            desc: this.get('desc'),
+            completed: false,
+        });
+
+		this.set('desc', '');
 	},
 	toggleAll: function(event) {
 		event.original.preventDefault();
@@ -21,23 +25,60 @@ var FormAdd = Ractive.extend({
 	},
 });
 
+var Items = Ractive.extend({
+    template: '#items-template',
+    data: {
+        items: [],
+        itemEditing: {i: null, desc: null},
+    },
+    editItem: function (event, i) {
+        event.original.preventDefault();
+
+        this.set('itemEditing', {
+            i: i,
+            desc: this.get('items.'+i+'.desc'),
+        });
+    },
+    submitEditItem: function (event) {
+        event.original.preventDefault();
+
+        var editing = this.get('itemEditing');
+
+        if ( ! editing.desc) return;
+
+        this.set('items.'+editing.i+'.desc', editing.desc);
+        this.set('itemEditing', {i: null, desc: null});
+    },
+    cancelEditItem: function (event) {
+        event.original.preventDefault();
+
+        this.set('itemEditing', {i: null, desc: null});
+    },
+    removeItem: function (event, i) {
+        event.original.preventDefault();
+
+        this.splice('items', i, 1);
+    },
+})
+
 var todos = new Ractive({
 	el: '#todos',
 	template: '#todos-template',
 	components: {
 		formAdd: FormAdd,
+		items: Items,
 	},
-	data: {
-		items: [],
-	},
+	data: {items: [
+        {desc: 'aaaaaaaa'},
+        {desc: 'bbbbbbbb'},
+    ]},
 });
 
 todos.on({
 	addItem: function(item) {
-		this.push('items', {item: item});
-		console.log(this.get('items'));
+		this.push('items', item);
 	},
 	toggleAll: function(status) {
-		console.log(status);
+		this.set('items.*.completed', status);
 	},
 });
