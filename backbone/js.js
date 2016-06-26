b085b96a64f879b0c42289c319a8516483bbd437
sontung0 <sontung0@gmail.com>
2016-06-26 22:56:49 +0700

    Update code

---
+++ backbone/js.js
@@ -0,0 +1,31 @@
+
+var User = Backbone.Model.extend({
+	defaults: {
+		name: 'Name',
+		desc: '<b>desc</b>',
+	},
+	url: './data.json',
+	initialize: function() {
+	},
+	validate: function(attrs) {
+		if ( ! attrs.phone) {
+			return 'The phone is required';
+		}
+	}
+});
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'user': 'user',
+		'user/:id': 'userView',
+	},
+	user: function() {
+		alert('user');
+	},
+	userView: function(id) {
+		alert('userView: '+id);
+	}
+})
+
+var user = new User();
+var router = new Router;
