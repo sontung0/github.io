a1ff9ae2bceef5dcbf09524ef92be9dd282d9bb6
Tung Nguyen Son <tung.nguyen.son@ntq-solution.com.vn>
2016-06-27 17:26:20 +0700

    Update code

--- backbone-users/js.js
+++ backbone-users/js.js
@@ -1,4 +1,9 @@
-var User = Backbone.Model.extend({});
+var User = Backbone.Model.extend({
+    validate: function(attrs) {
+        if ( ! attrs.name) return 'The name is required';
+        if ( ! attrs.phone) return 'The phone is required';
+    },
+});
 
 var Users = Backbone.Collection.extend({
     model: User,
@@ -15,18 +20,28 @@ var UserView = Backbone.View.extend({
     },
 
     render: function() {
-        this.$el.html(this.template(this.model.toJSON()));
+        this.$el.html(this.template({user: this.model}));
         return this;
     },
 });
 
-var AppView = Backbone.View.extend({
-    el: '#app',
-    data: {users: null},
+var UsersView = Backbone.View.extend({
+    template: _.template($('#users-template').html()),
+    data: {
+        users: null,
+        router: null,
+    },
 
     initialize: function(data) {
-        this.data = data;
+        _.extend(this.data, data);
         this.listenTo(this.data.users, 'add', this.addUser);
+        this.render();
+        this.data.users.fetch();
+    },
+
+    render: function() {
+        this.$el.html(this.template());
+        return this;
     },
 
     addUser: function(user) {
@@ -35,12 +50,101 @@ var AppView = Backbone.View.extend({
     },
 });
 
-var users = new Users;
-var appView = new AppView({
-    users: users,
+var UserFormView = Backbone.View.extend({
+    template: _.template($('#user-form-template').html()),
+    events: {
+        'submit form': 'submit',
+    },
+    data: {
+        user: null,
+        users: null,
+        router: null,
+    },
+
+    initialize: function(data) {
+        _.extend(this.data, data);
+        this.render(null);
+    },
+
+    render: function(userId) {
+        this.data.user = userId ? this.data.users.get(userId) : new User;
+        this.$el.html(this.template(this.data));
+        return this;
+    },
+
+    submit: function(e) {
+        e.preventDefault();
+
+        var user = new User({
+            name: this.$('[name=name]').val(),
+            phone: this.$('[name=phone]').val(),
+        });
+
+        if ( ! user.isValid()) {
+            this.$('[data-param=error]').html(user.validationError).show();
+            return;
+        }
+
+        if (this.data.user.isNew()) {
+            this.data.users.create(user.attributes);
+        } else {
+            this.data.user.save(user.attributes);
+        }
+
+        this.$('[data-param=error]').html('').hide();
+        this.data.router.redirect('list');
+    },
 });
 
-users.create({
-    name: 'Name',
-    phone: '123',
+var Router = Backbone.Router.extend({
+    routes: {
+        'list': 'listUsers',
+        'add': 'addUser',
+        'edit/:id': 'editUser',
+    },
+    data: {
+        app: null,
+        usersView: null,
+        userFormView: null,
+    },
+
+    initialize: function() {
+        var args = {
+            router: this,
+            users: new Users,
+        };
+
+        this.data.app = $('#app');
+        this.data.usersView = new UsersView(args);
+        this.data.userFormView = new UserFormView(args);
+
+        this.data.app.append(this.data.usersView.$el);
+        this.data.app.append(this.data.userFormView.$el);
+
+        this.listUsers();
+    },
+
+    listUsers: function() {
+        this.data.usersView.$el.show();
+        this.data.userFormView.$el.hide();
+    },
+
+    addUser: function() {
+        this.data.usersView.$el.hide();
+        this.data.userFormView.render(null).$el.show();
+    },
+
+    editUser: function(userId) {
+        this.data.usersView.$el.hide();
+        this.data.userFormView.render(userId).$el.show();
+    },
+
+    redirect: function(uri) {
+        this.navigate(uri, {trigger: true, replace: true});
+    },
+});
+
+$(function() {
+    var router = new Router;
+    Backbone.history.start();
 });
