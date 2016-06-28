9ea2bff972b2536141f11ce78c565713e9357e3b
Tung Nguyen Son <tung.nguyen.son@ntq-solution.com.vn>
2016-06-28 17:33:54 +0700

    Update code

--- d3-chart/grouped-bar/js.js
+++ d3-chart/grouped-bar/js.js
@@ -11,7 +11,7 @@ var y = d3.scale.linear()
     .range([height, 0]);
 
 var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    .range(["blue", "red"]);
 
 var xAxis = d3.svg.axis()
     .scale(x0)
@@ -28,28 +28,18 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("data.csv", function (error, data) {
+d3.csv("data.csv", function(error, data) {
     if (error) throw error;
 
-    var ageNames = d3.keys(data[0]).filter(function (key) {
-        return key !== "State";
+    var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+
+    data.forEach(function(d) {
+        d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
     });
 
-    data.forEach(function (d) {
-        d.ages = ageNames.map(function (name) {
-            return {name: name, value: +d[name]};
-        });
-    });
-
-    x0.domain(data.map(function (d) {
-        return d.State;
-    }));
+    x0.domain(data.map(function(d) { return d.State; }));
     x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-    y.domain([0, d3.max(data, function (d) {
-        return d3.max(d.ages, function (d) {
-            return d.value;
-        });
-    })]);
+    y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
 
     svg.append("g")
         .attr("class", "x axis")
@@ -70,36 +60,22 @@ d3.csv("data.csv", function (error, data) {
         .data(data)
         .enter().append("g")
         .attr("class", "state")
-        .attr("transform", function (d) {
-            return "translate(" + x0(d.State) + ",0)";
-        });
+        .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
 
     state.selectAll("rect")
-        .data(function (d) {
-            return d.ages;
-        })
+        .data(function(d) { return d.ages; })
         .enter().append("rect")
         .attr("width", x1.rangeBand())
-        .attr("x", function (d) {
-            return x1(d.name);
-        })
-        .attr("y", function (d) {
-            return y(d.value);
-        })
-        .attr("height", function (d) {
-            return height - y(d.value);
-        })
-        .style("fill", function (d) {
-            return color(d.name);
-        });
+        .attr("x", function(d) { return x1(d.name); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .style("fill", function(d) { return color(d.name); });
 
     var legend = svg.selectAll(".legend")
         .data(ageNames.slice().reverse())
         .enter().append("g")
         .attr("class", "legend")
-        .attr("transform", function (d, i) {
-            return "translate(0," + i * 20 + ")";
-        });
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
     legend.append("rect")
         .attr("x", width - 18)
@@ -112,8 +88,5 @@ d3.csv("data.csv", function (error, data) {
         .attr("y", 9)
         .attr("dy", ".35em")
         .style("text-anchor", "end")
-        .text(function (d) {
-            return d;
-        });
-
+        .text(function(d) { return d; });
 });
