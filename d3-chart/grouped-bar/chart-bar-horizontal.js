06980c73201ddf851ee7cdf7aeeb41aa688de39d
Tung Nguyen Son <tung.nguyen.son@ntq-solution.com.vn>
2016-07-01 15:13:21 +0700

    Update code

--- d3-chart/grouped-bar/chart-bar-horizontal.js
+++ d3-chart/grouped-bar/chart-bar-horizontal.js
@@ -21,50 +21,46 @@ function getData() {
 
 function draw(title, data) {
 	var svgWidth = window.innerWidth * 0.7;
-	var svgHeight = 700;
-	var margin = {top: 50, right: 20, bottom: 20, left: 150};
+	var svgHeight = 600;
+	var margin = {top: 20, right: 50, bottom: 20, left: 150};
 	var width = svgWidth - margin.left - margin.right;
 	var height = svgHeight - margin.top - margin.bottom;
-
-	var y0 = d3.scale.ordinal()
-		.rangeRoundBands([0, height], .1)
-		.domain(_.pluck(data, 'group'));
-
-	var y1 = d3.scale.ordinal()
-		.rangeRoundBands([0, y0.rangeBand()])
-		.domain(_.pluck(_.first(data).items, 'item'));
-
-	var yGroupLabel = d3.scale.ordinal()
-		.rangeRoundBands([0, y0.rangeBand()])
-		.domain([1, 2]);
 
 	var x = d3.scale.linear()
 		.range([0, width])
 		.domain([0, getItemValueMax()]);
 
-	var yAxis = d3.svg.axis()
-		.scale(y0)
-		.orient('left');
+	var y0 = d3.scale.ordinal()
+		.rangeRoundBands([height, 0], .1)
+		.domain(_.pluck(data, 'group'));
+
+	var y1 = d3.scale.ordinal()
+		.rangeRoundBands([0, y0.rangeBand()])
+		.domain(_.pluck(_.first(data).items, 'item').reverse());
+
+	var yGroupLabel = d3.scale.ordinal()
+		.rangeRoundBands([0, y0.rangeBand()])
+		.domain([1, 2]);
 
 	var xAxis = d3.svg.axis()
 		.scale(x)
 		.orient('bottom')
 		.tickFormat(d3.format('.2s'));
 
+	var yAxis = d3.svg.axis()
+		.scale(y0)
+		.orient('left');
+
 	var svg = makeSvg();
 
-	drawTitle();
 	drawAxis();
 	drawBars();
-	//drawListGroups('top');
-	//drawListGroups('bottom');
-	//drawListGroups('left');
-	//drawListGroups('right');
 
 	function makeSvg() {
 		return d3.select('#chart').append('svg')
 			.attr('width', svgWidth)
 			.attr('height', svgHeight)
+			.attr('class', 'chart-bar-horizonal')
 			.append('g')
 			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 	}
@@ -73,14 +69,30 @@ function draw(title, data) {
 		svg.append('g')
 			.attr('class', 'x axis')
 			.attr('transform', 'translate(0,' + height + ')')
-			.call(xAxis);
+			.call(xAxis)
+			.selectAll('.tick line')
+			.attr('y1', 5)
+			.attr('y2', function(d) {
+				return -height;
+			});
 
 		svg.append('g')
 			.attr('class', 'y axis')
 			.call(yAxis)
 			.selectAll('.tick text')
+			.attr('x', function(d) {
+				var group = _.findWhere(data, {group: d});
+				var xs = group.items.map(function(item) {
+					return x(item.value)
+				});
+
+				return d3.max(xs);
+			})
+			.style('text-anchor', 'start')
+			.attr('dx', '0.5em')
 			.text(function(d) {
 				var group = _.findWhere(data, {group: d});
+
 				return group.group + ' ' + _.pluck(group.items, 'value').join(',');
 			});
 	}
@@ -99,10 +111,10 @@ function draw(title, data) {
 				return d.items;
 			})
 			.enter().append('rect')
-			.attr('height', y1.rangeBand())
 			.attr('width', function(d) {
 				return x(d.value);
 			})
+			.attr('height', y1.rangeBand())
 			.attr('x', 0)
 			.attr('y', function(d) {
 				return y1(d.item);
@@ -110,55 +122,6 @@ function draw(title, data) {
 			.style('fill', function(d) {
 				return d.color;
 			});
-
-		//group.append('text')
-		//	.attr('x', function(d) {
-		//		var xs = d.items.map(function(item) {
-		//			return x(item.value)
-		//		});
-		//		return d3.min(xs);
-		//	})
-		//	.attr('x', xGroupLabel(2))
-		//	.attr('dy', '-2.5em')
-		//	.style('text-anchor', 'middle')
-		//	.text(function(d) {
-		//		return d.group
-		//	});
-		//
-		//group.append('text')
-		//	.attr('x', xGroupLabel(2))
-		//	.attr('y', function(d) {
-		//		var ys = d.items.map(function(item) {
-		//			return y(item.value)
-		//		});
-		//		return d3.min(ys);
-		//	})
-		//	.attr('dy', '-1em')
-		//	.style('text-anchor', 'middle')
-		//	.text(function(d) {
-		//		return _.pluck(d.items, 'value');
-		//	});
-	}
-
-	function drawListGroups(location) {
-		var items = _.first(data).items;
-
-		d3.select('#chart-bar-groups-' + location).html(function() {
-			var html = '<div class="list-groups">';
-			items.forEach(function(item) {
-				html += '<div class="group-item">'
-					+ '<span class="group-item-color" style="background-color:' + item.color + ';"></span>'
-					+ '<span class="group-item-label">' + item.item + '</span>'
-					+ '</div>';
-			});
-			html += '</div>';
-
-			return html;
-		});
-	}
-
-	function drawTitle() {
-		d3.select('#chart-bar-title').html(title);
 	}
 
 	function getItemValueMax() {
